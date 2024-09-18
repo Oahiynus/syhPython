0ceb9433869a1da1f83323b1e22d6c2c1eaf75f4
Oahiynus <yh-sun@foxmail.com>
2024-09-18 10:21:47 +0800

    添加开源课程代码

--- OpenSourceGIS/module.py
+++ OpenSourceGIS/module.py
@@ -1,11 +1,11 @@
-def num_higher_(s,s_high):
+def num_higher_(s,s_high=90):
     num=0
     for a in s:
         if a >= s_high:
             num = num+1
     return num
 
-def num_lower_(s,s_low):
+def num_lower_(s,s_low=70):
     num=0
     for a in s:
         if a < s_low:

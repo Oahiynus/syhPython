9b488961d0a0f3bf2e72e0788c3f5d495ee13bfb
Oahiynus <yh-sun@foxmail.com>
2024-09-24 19:52:27 +0800

    更新PythonLearning

---
+++ PythonLearning/Python入口.py
@@ -0,0 +1,11 @@
+
+a_list = [1, 2, 3, 4, 5]
+a_list.append(6)
+a_list.append(7)
+a_list.append(8)
+a_list.append(9)
+a_list.append(10)
+
+#Python的入口（不是主函数）
+if __name__ == '__main__':
+    print('hello')

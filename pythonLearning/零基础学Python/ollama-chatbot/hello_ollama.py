fd2e0c069d2926ffb38e44a2435487724db3a3af
Oahiynus <yh-sun@foxmail.com>
2024-11-13 12:34:32 +0800

    新增网页功能相关依赖库。修改聊天内容示例为中文文件。

--- pythonLearning/零基础学Python/ollama-chatbot/hello_ollama.py
+++ pythonLearning/零基础学Python/ollama-chatbot/hello_ollama.py
@@ -2,7 +2,7 @@ import ollama
 response = ollama.chat(model='llama3.2', messages=[
   {
     'role': 'user',
-    'content': 'Why is the sky blue?',
+    'content': '大海为什么是蓝色的？',
   },
 ])
 print(response['message']['content'])
--- pythonLearning/零基础学Python/ollama-chatbot/hello_ollama_stream.py
+++ pythonLearning/零基础学Python/ollama-chatbot/hello_ollama_stream.py
@@ -2,7 +2,7 @@ import ollama
 
 stream = ollama.chat(
     model='llama3.2',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    messages=[{'role': 'user', 'content': '大海为什么是蓝色的？'}],
     stream=True,
 )
 
